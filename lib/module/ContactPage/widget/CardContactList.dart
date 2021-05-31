cb047b3f2144c7feb59e615cd24fd99220933e5b
saepulawr <adm.ipul@gmail.com>
2021-06-01 02:17:44 +0700

    fix delete action

--- lib/module/ContactPage/widget/CardContactList.dart
+++ lib/module/ContactPage/widget/CardContactList.dart
@@ -1,3 +1,4 @@
+import 'package:adaptive_dialog/adaptive_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:testptgic/module/ContactPage/ContactPage.dart';
@@ -6,10 +7,16 @@ import 'package:testptgic/module/ContactPage/model/ModelContactAll.dart'
     as modelContact;
 import 'package:testptgic/module/Function/PublicFunction.dart';
 
-class CardContactList extends StatelessWidget {
+class CardContactList extends StatefulWidget {
   final modelContact.Contact contact;
-  const CardContactList({required this.contact, Key? key}) : super(key: key);
+  CardContactList({Key? key, required this.contact}) : super(key: key);
 
+  @override
+  _CardContactListState createState() => _CardContactListState();
+}
+
+class _CardContactListState extends State<CardContactList> {
+  bool showDetail = false;
   @override
   Widget build(BuildContext context) {
     return Slidable(
@@ -19,10 +26,20 @@ class CardContactList extends StatelessWidget {
         color: Colors.white,
         child: ListTile(
           leading: CircleAvatar(
-            child: Text(getInitial(contact.nama!)),
+            child: Text(getInitial(widget.contact.nama!)),
           ),
-          title: Text(contact.nama!),
-          onTap: () {},
+          title: Text(
+            widget.contact.nama!,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: showDetail
+              ? Text(widget.contact.telp! + " - " + widget.contact.email!)
+              : null,
+          onTap: () {
+            setState(() {
+              showDetail = !showDetail;
+            });
+          },
         ),
       ),
       actions: <Widget>[
@@ -36,15 +53,38 @@ class CardContactList extends StatelessWidget {
               FunctionContact().showFormContact(
                   context: context,
                   isEdit: true,
-                  idOldData: int.parse(contact.id!),
-                  contact: contact,
+                  idOldData: int.parse(widget.contact.id!),
+                  contact: widget.contact,
                   onSuccess: () => keyContact.currentState!.handleRefresh());
             }),
         IconSlideAction(
             caption: 'Delete',
             color: Colors.red,
             icon: Icons.delete,
-            onTap: () {}),
+            onTap: () async {
+              final hasil = await showOkCancelAlertDialog(
+                  context: context,
+                  title: "Delete this contact ?",
+                  message: "This action cannot undone");
+              if (hasil == OkCancelResult.ok) {
+                showOverlayLoading(context);
+                if (await FunctionContact()
+                    .deleteContact(id: int.parse(widget.contact.id!))) {
+                  //sukses
+                  SnackBar(
+                    backgroundColor: Colors.green,
+                    content: Text("Delete data success"),
+                  ).show(context);
+                } else {
+                  //gagal
+                  SnackBar(
+                    backgroundColor: Colors.red,
+                    content: Text("Error when delete data!"),
+                  ).show(context);
+                }
+                hideOverlayLoading();
+              }
+            }),
       ],
     );
   }
